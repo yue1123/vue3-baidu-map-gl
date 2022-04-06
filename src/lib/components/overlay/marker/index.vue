6b6400f47e82159d3e7aaa44ae743fd73ab25de6
花开半亩地 <2572468699@qq.com>
2022-04-06 18:17:21 +0800

    feat: 增加覆盖物组件

---
+++ src/lib/components/overlay/marker/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<slot></slot>
+</template>
+
+<script setup lang="ts">
+	import { defineProps, withDefaults } from 'vue'
+	 type a = BMapGL.MarkerOptions extends {offset:any}
+	const props = withDefaults(defineProps<a>(), {
+	   /**
+		 * 标注的位置偏移值
+		 */
+		offset: {
+			x: number
+			y: number
+		}
+		/**
+		 * 标注所用的图标对象
+		 */
+		icon: Icon,
+		enableMassClear: true,
+		enableDragging: false,
+		enableClicking: true,
+		raiseOnDrag: false,
+		draggingCursor: 'pointer',
+		rotation: 0,
+		title: ''
+	 })
+</script>
