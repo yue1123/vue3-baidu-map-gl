95d7f4a0e095799614d531f34febb17b49b8dd04
花开半亩地 <2572468699@qq.com>
2022-04-08 00:40:03 +0800

    chore: 构建配置测试

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,28 +1,83 @@
-// rollup.config.js
-import resolve from 'rollup-plugin-node-resolve'
-import vue from 'rollup-plugin-vue'
-import babel from '@rollup/plugin-babel'
-import typescript from '@rollup/plugin-typescript'
-const config = {
-	input: './src/index.ts', // 必须，入口文件
-	output: {
-		// 必须，输出文件 (如果要输出多个，可以是一个数组)
-		exports: 'named', // 输出多个文件
-		globals: {
-			vue: 'Vue' // 告诉rollup全局变量Vue即是vue
+// // rollup.config.js
+const resolve = require( 'rollup-plugin-node-resolve')
+const { terser } = require('rollup-plugin-terser')
+const { uglify } = require('rollup-plugin-uglify')
+const pkg = require('./package')
+const fs = require('fs')
+const path = require('path')
+const vue  = require('rollup-plugin-vue')
+const typescript = require('rollup-plugin-typescript')
+
+
+const deps = Object.keys(pkg.dependencies || {})
+
+
+// /packages/button/rollup.config.js
+// import { terser } from 'rollup-plugin-terser'
+const readDirectory = (dir) => {
+	//递归目录结构
+	const list = fs.readdirSync(dir)
+	const ret = []
+	list.forEach((filename) => {
+		const dist = path.resolve(dir, filename)
+		const stat = fs.statSync(dist)
+		if (stat.isFile()) {
+			if (filename === 'index.vue' || filename.endsWith('.ts')) {
+				ret.push(dist)
+			}
+		} else {
+			ret.push(...readDirectory(dist))
 		}
-	},
-	plugins: [
-		// 引入的插件在这里配置
-		typescript(),
-		vue({
-			exposeFilename: true
-		}),
-		resolve(),
-		babel({
-			exclude: '**/node_modules/**'
-		})
-	]
+	})
+	return ret
 }
 
-export default config
+const componentsBuild = readDirectory(path.resolve(__dirname, './src/lib/components')).map((component) => {
+	return {
+		external(id) {
+			if (/^mitt/.test(id)) {
+				return false
+			}
+			return /^vue/.test(id) || /hooks/.test(id) || deps.some((k) => new RegExp('^' + k).test(id))
+		},
+		// 入口
+		input: component,
+		// 出口
+		output: [
+			{
+				// 必须，输出文件 (如果要输出多个，可以是一个数组)
+				exports: 'named', // 输出多个文件
+				file: component.replace('src', 'dist').replace('.vue', '.js'), // 必须，输出文件
+				format: 'es'
+			}
+		],
+		// 插件
+		plugins: [resolve(), vue(), typescript()]
+	}
+})
+console.log(readDirectory(path.resolve(__dirname, './src/hooks')))
+const hooksBuild = readDirectory(path.resolve(__dirname, './src/hooks')).map((hooks) => {
+	return {
+		external(id) {
+			if (/^mitt/.test(id)) {
+				return false
+			}
+			return /^vue/.test(id) || /hooks/.test(id) || deps.some((k) => new RegExp('^' + k).test(id))
+		},
+		// 入口
+		input: hooks,
+		// 出口
+		output: [
+			{
+				// 必须，输出文件 (如果要输出多个，可以是一个数组)
+				exports: 'named', // 输出多个文件
+				file: hooks.replace('src', 'dist').replace('.ts', '.js'), // 必须，输出文件
+				format: 'es'
+			}
+		],
+		// 插件
+		plugins: [resolve(), typescript()]
+	}
+})
+
+module.exports = [...componentsBuild, ...hooksBuild]
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,92 @@
-const { join, resolve } = require('path')
+const { join, resolve, dirname } = require('path')
 import nodeResolve from 'rollup-plugin-node-resolve'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const root = process.cwd()
 
+
+import fs from 'fs/promises'
+import { createFilter } from '@rollup/pluginutils'
+import { normalizePath } from 'vite'
+import { Project } from 'ts-morph'
+
+import type { Plugin } from 'vite'
+import type { SourceFile } from 'ts-morph'
+
+const genDTypes = (): Plugin => {
+	const filter = createFilter(['**/*.vue', '**/*.ts'], 'node_modules/**')
+	const sourceFiles: SourceFile[] = []
+
+	const project = new Project({
+		compilerOptions: {
+			declaration: true,
+			emitDeclarationOnly: true,
+			noEmitOnError: true,
+			allowJs: true, // 如果想兼容 js 语法需要加上
+			outDir: 'dist' // 可以设置自定义的打包文件夹，如 'types'
+		},
+		tsConfigFilePath: resolve(__dirname, 'tsconfig.json'),
+		skipAddingFilesFromTsConfig: true
+	})
+
+	let root: string
+
+	return {
+		name: 'gen-dts',
+		apply: 'build',
+		enforce: 'pre', // 需要在 pre 才能正确拿到 ts 的 script 部分
+		configResolved(config) {
+			root = config.root
+		},
+		transform(code, id) {
+			if (!code || !filter(id)) return null
+
+			// 拆分后的文件 id 具有一些特征，可以用正则的方式来捕获
+			if (/\.vue(\?.*type=script.*)$/.test(id)) {
+				const filePath = resolve(root, normalizePath(id.split('?')[0]))
+
+				sourceFiles.push(project.createSourceFile(filePath + (/lang.ts/.test(id) ? '.ts' : '.js'), code))
+			} else if (/\.ts$/.test(id)) {
+				const filePath = resolve(root, normalizePath(id))
+
+				sourceFiles.push(project.addSourceFileAtPath(filePath))
+			}
+		},
+		async generateBundle() {
+			const diagnostics = project.getPreEmitDiagnostics()
+
+			// 输出解析过程中的错误信息
+			console.log(project.formatDiagnosticsWithColorAndContext(diagnostics))
+
+			project.emitToMemory()
+
+			// 随后将解析完的文件写道打包路径
+			for (const sourceFile of sourceFiles) {
+				const emitOutput = sourceFile.getEmitOutput()
+
+				for (const outputFile of emitOutput.getOutputFiles()) {
+					const filePath = outputFile.getFilePath()
+
+					await fs.mkdir(dirname(filePath), { recursive: true })
+					await fs.writeFile(filePath, outputFile.getText(), 'utf8')
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+
 export default defineConfig({
 	root,
-	plugins: [vue({ customElement: true, reactivityTransform: true })],
+	plugins: [
+		vue(),
+		nodeResolve()
+		// genDTypes()
+	],
 	resolve: {
 		alias: {
 			hooks: join(root, 'src/hooks/'),
@@ -15,10 +95,11 @@ export default defineConfig({
 		}
 	},
 	build: {
+		outDir: './dist',
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'MyLib',
-			fileName: (format) => `my-lib.${format}.js`
+			name: 'MyLib'
+			// fileName: (format) => `my-lib.${format}.js`
 		},
 		rollupOptions: {
 			// 确保外部化处理那些你不想打包进库的依赖
