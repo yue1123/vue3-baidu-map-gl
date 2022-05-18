1cfb04480fe9de84543631ee40d216c279630d04
花开半亩地 <2572468699@qq.com>
2022-05-18 14:12:50 +0800

    docs: docs build

--- docs/dist/assets/useBaseMapEffect.b96d95c9.js
+++
@@ -1,1 +0,0 @@
-import{i,a as c}from"./app.d9123eef.js";import{a as p}from"./useLife.59d9b310.js";function r(e){const{on:s}=p(),t=i("getMapInstance"),a=t&&t();let n;a?n=e(a):s("initd",o=>{n=e(o)}),c(()=>{n&&n()})}export{r as u};
