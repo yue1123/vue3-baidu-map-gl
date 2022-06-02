8c75b699b74367f3635f6ec10f0cc848996c002f
花开半亩地 <2572468699@qq.com>
2022-06-03 00:10:56 +0800

    chore: 移除 docs 构建文件夹版本管理

--- docs/dist/assets/bindEvents.81d30a10.js
+++
@@ -1,1 +0,0 @@
-function c(t,o,s){for(const e of Object.keys(t))if(/^on/.test(e)&&t[e]){let n=e.replace(/^on/,"").toLocaleLowerCase();s.addEventListener(n,a=>o(n,a))}}export{c as b};
--- docs/dist/assets/index.html.9bd47afe.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./app.e8329c2e.js";const _={};function r(n,t){return null}var f=e(_,[["render",r],["__file","index.html.vue"]]);export{f as default};
