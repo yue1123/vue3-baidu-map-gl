a92793cc40ab57fdb8d597b8d2a3269156970d20
花开半亩地 <2572468699@qq.com>
2022-10-22 17:06:06 +0800

    chore: 移除无用 dev依赖, 增加 prettier 格式化

--- rollup.config.js
+++ rollup.config.js
@@ -5,63 +5,62 @@ const resolve = require('rollup-plugin-node-resolve')
 const babel = require('@rollup/plugin-babel').default
 const replace = require('@rollup/plugin-replace')
 const { terser } = require('rollup-plugin-terser')
-// const externals = require('rollup-plugin-node-externals').default
 const vue = require('rollup-plugin-vue')
 const typescript = require('rollup-plugin-typescript')
 const pkg = require('./package.json')
 
 const baseConfig = defineConfig({
-	external: ['vue'],
-	input: path.resolve('./packages/index.ts'),
-	plugins: [
-		replace({
-			values: {
-				__VERSION__: pkg.version
-			},
-			preventAssignment: true
-		}),
-		typescript(),
-		vue(),
-		resolve(),
-		babel({
-			exclude: 'node_modules/**',
-			babelHelpers: 'bundled'
-		})
-	],
-	output: {
-		name: 'Vue3baiduMapGl',
-		format: 'umd',
-		exports: 'named',
-		globals: {
-			vue: 'Vue'
-		}
-	}
+  external: ['vue'],
+  input: path.resolve('./packages/index.ts'),
+  plugins: [
+    replace({
+      values: {
+        __VERSION__: pkg.version
+      },
+      preventAssignment: true
+    }),
+    typescript(),
+    vue(),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
+    })
+  ],
+  output: {
+    name: 'Vue3baiduMapGl',
+    format: 'umd',
+    exports: 'named',
+    globals: {
+      vue: 'Vue'
+    }
+  }
 })
 
 const devConfig = defineConfig({
-	output: {
-		file: path.resolve('dist/index.js')
-	}
+  output: {
+    file: path.resolve('dist/index.js')
+  }
 })
 
 const prodConfig = defineConfig({
-	plugins: [
-		replace({
-			values: {
-				'process.env.NODE_ENV': JSON.stringify('production')
-			},
-			preventAssignment: true
-		}),
-		terser()
-	],
-	output: {
-		file: path.resolve('dist/index.prod.js')
-	}
+  plugins: [
+    replace({
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('production')
+      },
+      preventAssignment: true
+    }),
+    terser()
+  ],
+  output: {
+    file: path.resolve('dist/index.prod.js')
+  }
 })
 
 module.exports = [
-	// dev umd output
-	merge(baseConfig, devConfig),
-	// prod umd output
-	merge(baseConfig, prodConfig),
+  // dev umd output
+  merge(baseConfig, devConfig),
+  // prod umd output
+  merge(baseConfig, prodConfig)
 ]
