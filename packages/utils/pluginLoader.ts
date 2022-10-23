fc9646d66c6fb8a5bed9d37baadd2625ea610c95
花开半亩地 <2572468699@qq.com>
2022-10-24 01:02:37 +0800

    feat: 增加 mapvgl 和 mapv 插件

--- packages/utils/pluginLoader.ts
+++ packages/utils/pluginLoader.ts
@@ -2,7 +2,7 @@
 
 import getScriptAsync from './getScriptAsync'
 
-export type PluginsList = ['TrackAnimation', 'MapVGl']
+export type PluginsList = ['TrackAnimation', 'Mapvgl', 'Mapv']
 export type PluginsUnion = PluginsList[number]
 export type PluginsSourceLink = Record<PluginsUnion, string>
 export type PluginsLoader = (...args: any[]) => Promise<any>
@@ -10,7 +10,8 @@ export type UserPlugins = (PluginsUnion | PluginsLoader)[]
 
 export const DEFAULT_PLUGINS_SOURCE_LINK: PluginsSourceLink = {
   TrackAnimation: '//mapopen.bj.bcebos.com/github/BMapGLLib/TrackAnimation/src/TrackAnimation.min.js',
-  MapVGl: 'https://unpkg.com/mapvgl/dist/mapvgl.min.js'
+  Mapvgl: 'https://code.bdstatic.com/npm/mapvgl@1.0.0-beta.159/dist/mapvgl.min.js',
+  Mapv: '//mapv.baidu.com/build/mapv.min.js'
 } as const
 
 export const pluginLoaderMap: Record<PluginsUnion, (customSourceLink?: string) => Promise<any>> = {
@@ -20,11 +21,17 @@ export const pluginLoaderMap: Record<PluginsUnion, (customSourceLink?: string) =
       addCalToWindow: false,
       key: 'trackAnimation'
     }),
-  MapVGl: (customSourceLink?: string) =>
+  Mapv: (customSourceLink?: string) =>
     getScriptAsync({
-      src: customSourceLink || DEFAULT_PLUGINS_SOURCE_LINK['TrackAnimation'],
+      src: customSourceLink || DEFAULT_PLUGINS_SOURCE_LINK['Mapvgl'],
       addCalToWindow: false,
-      key: 'trackAnimation'
+      key: 'Mapvgl'
+    }),
+  Mapvgl: (customSourceLink?: string) =>
+    getScriptAsync({
+      src: customSourceLink || DEFAULT_PLUGINS_SOURCE_LINK['Mapv'],
+      addCalToWindow: false,
+      key: 'Mapv'
     })
 }
 
