f68cd611e0223b41e60a491538bb46265feebc15
花开半亩地 <2572468699@qq.com>
2023-03-07 17:10:06 +0800

    fix: marker icon type warning

--- packages/components/overlay/marker/index.vue
+++ packages/components/overlay/marker/index.vue
@@ -5,8 +5,36 @@
   import useBaseMapEffect from '../../../hooks/useBaseMapEffect'
   import { bindEvents, Callback, isString, callWhenDifferentValue, isDef, Point } from '../../../utils/index'
   import useLifeCycle from '../../../hooks/useLifeCycle'
-  import { useDefaultMarkerIcons, DefaultMarkerIcons } from '../../../hooks/useDefaultMarkerIcons'
+  import { useDefaultMarkerIcons } from '../../../hooks/useDefaultMarkerIcons'
 
+  export type MarkerIcons =
+    | 'simple_red'
+    | 'simple_blue'
+    | 'loc_red'
+    | 'loc_blue'
+    | 'start'
+    | 'end'
+    | 'location'
+    | 'red1'
+    | 'red2'
+    | 'red3'
+    | 'red4'
+    | 'red5'
+    | 'red6'
+    | 'red7'
+    | 'red8'
+    | 'red9'
+    | 'red10'
+    | 'blue1'
+    | 'blue2'
+    | 'blue3'
+    | 'blue4'
+    | 'blue5'
+    | 'blue6'
+    | 'blue7'
+    | 'blue8'
+    | 'blue9'
+    | 'blue10'
   export interface MarkerOffset {
     x: number
     y: number
@@ -36,7 +64,7 @@
     /**
      * 标注所用的图标对象
      */
-    icon?: MarkerCustomIcon | DefaultMarkerIcons
+    icon?: MarkerIcons | MarkerCustomIcon
     /**
      * 显示层级
      */
--- packages/hooks/useDefaultMarkerIcons.ts
+++ packages/hooks/useDefaultMarkerIcons.ts
@@ -1,35 +1,6 @@
-export type DefaultMarkerIcons =
-  | 'simple_red'
-  | 'simple_blue'
-  | 'loc_red'
-  | 'loc_blue'
-  | 'start'
-  | 'end'
-  | 'location'
-  | 'red1'
-  | 'red2'
-  | 'red3'
-  | 'red4'
-  | 'red5'
-  | 'red6'
-  | 'red7'
-  | 'red8'
-  | 'red9'
-  | 'red10'
-  | 'blue1'
-  | 'blue2'
-  | 'blue3'
-  | 'blue4'
-  | 'blue5'
-  | 'blue6'
-  | 'blue7'
-  | 'blue8'
-  | 'blue9'
-  | 'blue10'
+let icons: Record<string, BMapGL.Icon> | null = null
 
-let icons: Record<DefaultMarkerIcons, BMapGL.Icon> | null = null
-
-export function useDefaultMarkerIcons(): Record<DefaultMarkerIcons, BMapGL.Icon> {
+export function useDefaultMarkerIcons(): Record<string, BMapGL.Icon> {
   if (icons !== null) return icons
   const defaultIconUrl = '//mapopen.bj.bcebos.com/cms/react-bmap/markers_new2x_fbb9e99.png'
   icons = {
@@ -61,7 +32,7 @@ export function useDefaultMarkerIcons(): Record<DefaultMarkerIcons, BMapGL.Icon>
       imageOffset: new BMapGL.Size(248 / 2, 466 / 2),
       imageSize: new BMapGL.Size(600 / 2, 600 / 2)
     })
-  } as Record<DefaultMarkerIcons, BMapGL.Icon>
+  } as Record<string, BMapGL.Icon>
 
   for (let i = 1; i <= 10; i++) {
     icons['red' + i] = new BMapGL.Icon(defaultIconUrl, new BMapGL.Size(42 / 2, 66 / 2), {
