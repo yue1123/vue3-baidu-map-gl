901845d61480c3be59f9d2fa10148eedf3704191
yue1123 <2572468699@qq.com>
2023-06-20 00:55:14 +0800

    feat: navigation3d 支持visible属性

--- docs/zh-CN/components/control/navigation3d.md
+++ docs/zh-CN/components/control/navigation3d.md
@@ -19,6 +19,12 @@ control/navigation3d
 | anchor | 控件的停靠位置 | `string`                  | [anchor](#anchor) | `BMAP_ANCHOR_BOTTOM_LEFT` |
 | offset | 控件的偏移值   | `{x: number, y: number }` | -                 | `{ x: 83, y: 18 }`        |
 
+## 动态组件 Props
+
+| 属性    | 说明     | 类型      | 可选值 | 默认值 | 版本                               |
+| ------- | -------- | --------- | ------ | ------ | ---------------------------------- |
+| visible | 是否显示 | `boolean` | -      | `true` | <Badge type="tip" text="^2.1.4" /> |
+
 ## anchor
 
 | 值                       | 说明 |
--- packages/components/control/navigation3d/index.vue
+++ packages/components/control/navigation3d/index.vue
@@ -1,6 +1,7 @@
 <template></template>
 
 <script setup lang="ts">
+  import { watch } from 'vue'
   import useBaseMapEffect from '../../../hooks/useBaseMapEffect'
   import useLifeCycle from '../../../hooks/useLifeCycle'
   import { type ControlAnchor } from '../../../utils'
@@ -16,21 +17,33 @@
       x: number
       y: number
     }
+    /**
+     * 是否可见
+     */
+    visible?: boolean
   }
   const props = withDefaults(defineProps<Navigation3dProps>(), {
     anchor: 'BMAP_ANCHOR_BOTTOM_RIGHT',
-    offset: () => ({ x: 83, y: 18 })
+    offset: () => ({ x: 83, y: 18 }),
+    visible: true
   })
   const { ready } = useLifeCycle()
   let navigation3dControl: BMapGL.NavigationControl3D
   defineEmits(['initd', 'unload'])
   useBaseMapEffect((map) => {
+    const { visible, offset, anchor } = props
     navigation3dControl = new BMapGL.NavigationControl3D({
-      offset: new BMapGL.Size(props.offset.x, props.offset.y),
-      anchor: window[props.anchor]
+      offset: new BMapGL.Size(offset.x, offset.y),
+      anchor: window[anchor]
     })
-    map.addControl(navigation3dControl)
+    visible && map.addControl(navigation3dControl)
     ready(map, navigation3dControl)
+    watch(
+      () => props.visible,
+      (n) => {
+        map[n ? 'addControl' : 'removeControl'](navigation3dControl)
+      }
+    )
     return () => map.removeControl(navigation3dControl)
   })
   defineOptions({
