ffccbccc2d64a0f015f1889fe1eb018d4d58738b
yue1123 <2572468699@qq.com>
2023-06-20 00:56:15 +0800

    feat: scale 支持visible属性

--- packages/components/control/scale/index.vue
+++ packages/components/control/scale/index.vue
@@ -21,34 +21,47 @@
      * 比例尺单位制
      */
     unit?: LengthUnit
+    /**
+     * 是否可见
+     */
+    visible?: boolean
   }
   const { ready } = useLifeCycle()
   const props = withDefaults(defineProps<ScaleProps>(), {
     anchor: 'BMAP_ANCHOR_BOTTOM_LEFT',
     offset: () => ({ x: 83, y: 18 }),
-    unit: 'BMAP_UNIT_METRIC'
+    unit: 'BMAP_UNIT_METRIC',
+    visible: true
   })
   let scaleCtrl: BMapGL.ScaleControl
   defineEmits(['initd', 'unload'])
   useBaseMapEffect((map) => {
+    const { visible, offset, anchor } = props
     scaleCtrl = new BMapGL.ScaleControl({
-      offset: new BMapGL.Size(props.offset.x, props.offset.y),
-      anchor: window[props.anchor]
+      offset: new BMapGL.Size(offset.x, offset.y),
+      anchor: window[anchor]
     })
+    visible && map.addControl(scaleCtrl)
     setUnit()
-    map.addControl(scaleCtrl)
     ready(map, scaleCtrl)
+    watch(
+      () => props.visible,
+      (n) => {
+        map[n ? 'addControl' : 'removeControl'](scaleCtrl)
+      }
+    )
     return () => map.removeControl(scaleCtrl)
   })
   // 监听比例尺单位变化
   watch(() => props.unit, setUnit)
+  defineOptions({
+    name: 'BScale'
+  })
+
   /**
    * 设置比例尺单位制
    */
   function setUnit() {
     scaleCtrl.setUnit(window[props.unit])
   }
-  defineOptions({
-    name: 'BScale'
-  })
 </script>
