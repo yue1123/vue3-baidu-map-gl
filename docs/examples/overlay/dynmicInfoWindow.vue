092df03e57ba578704e37caf34cc0d764ba066ca
yue1123 <2572468699@qq.com>
2023-12-03 01:30:38 +0800

    docs(infoWindow): 修正文档示例中 bug

--- docs/examples/overlay/dynmicInfoWindow.vue
+++ docs/examples/overlay/dynmicInfoWindow.vue
@@ -1,13 +1,23 @@
 <template>
   <BMap enableScrollWheelZoom v-bind="$attrs" :center="{ lat: 39.915185, lng: 116.400901 }" :zoom="16">
     <BMarker
-      v-for="item in markers"
+      v-for="(item, index) in markers"
       :position="item.position"
-      icon="blue1"
+      :icon="`blue${index + 1 as 1 | 2}`"
       @click="() => handleClick(item)"
       enableClicking
     />
-    <BInfoWindow v-model:show="show" :position="position" :title="title" enableAutoPan enableCloseOnClick />
+    <BInfoWindow
+      v-model:show="show"
+      :position="position"
+      :title="title"
+      enableAutoPan
+      enableCloseOnClick
+      :offset="{
+        x: 0,
+        y: -10
+      }"
+    />
   </BMap>
 </template>
 
@@ -16,11 +26,11 @@
   import { BMap, BMarker, BInfoWindow } from 'vue3-baidu-map-gl'
   let markers = ref([
     {
-      position: { lat: 39.915185, lng: 116.400901 },
+      position: { lat: 39.915185, lng: 116.400101 },
       title: '地址一'
     },
     {
-      position: { lat: 39.915185, lng: 116.400101 },
+      position: { lat: 39.915185, lng: 116.400901 },
       title: '地址二'
     }
   ])
